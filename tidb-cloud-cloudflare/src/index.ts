89eb9ae425d7324161f70d8e7fd013adf6f4c0f8
bohnen <bohnen@users.noreply.github.com>
2024-02-19 09:17:01 +0000

    READMEの修正、SQL Toolsの追加

--- tidb-cloud-cloudflare/src/index.ts
+++ tidb-cloud-cloudflare/src/index.ts
@@ -9,6 +9,8 @@ export default {
    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
       const conn = connect({url:env.DATABASE_URL})
       const resp = await conn.execute("show databases")
+      /* テーブル作成後、下記部分のコメントを解除して再デプロイ */
+      // const resp = await conn.execute("select * from `bookshop`.`users`;")
       return new Response(JSON.stringify(resp));
    },
 };
