bdf1ecad13156cc20ebaaea3c5f31dd67494f21e
bohnen <bohnen.net@gmail.com>
2024-04-23 12:27:46 +0900

    finish momento cache

--- client-sdk-javascript/examples/cloudflare-workers/http-api/src/worker.ts
+++ client-sdk-javascript/examples/cloudflare-workers/http-api/src/worker.ts
@@ -9,19 +9,15 @@
  */
 
 class MomentoFetcher {
-	private readonly apiKey: string;
+	private readonly apiToken: string;
 	private readonly baseurl: string;
-	constructor(key: string, endpoint: string) {
-		this.apiKey = key;
-		if (!endpoint.startsWith('https://')) {
-			this.baseurl = `https://${endpoint}/cache`;
-		} else {
-			this.baseurl = `${endpoint}/cache`;
-		}
+	constructor(token: string, endpoint: string) {
+		this.apiToken = token;
+		this.baseurl = `${endpoint}/cache`;
 	}
 
 	async get(cacheName: string, key: string) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiKey}`);
+		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}`);
 		if (resp.status < 300) {
 			console.log(`successfully retrieved ${key} from cache`)
 		} else {
@@ -32,7 +28,7 @@ class MomentoFetcher {
 	}
 
 	async set(cacheName: string, key: string, value: string, ttl_seconds: number = 30) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiKey}&&ttl_seconds=${ttl_seconds}`, {
+		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}&&ttl_seconds=${ttl_seconds}`, {
 			method: 'PUT',
 			body: value
 		});
@@ -47,7 +43,7 @@ class MomentoFetcher {
 	}
 
 	async delete(cacheName: string, key: string) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiKey}`, {
+		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}`, {
 			method: 'DELETE',
 		});
 		if (resp.status < 300) {
@@ -61,31 +57,24 @@ class MomentoFetcher {
 }
 
 export interface Env {
-	MOMENTO_API_KEY: string;
-	MOMENTO_HTTP_ENDPOINT: string;
+	MOMENTO_AUTH_TOKEN: string;
+	MOMENTO_REST_ENDPOINT: string;
 	MOMENTO_CACHE_NAME: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-
-		if (env.MOMENTO_API_KEY === undefined) {
-			throw new Error('MOMENTO_API_KEY must be set in .dev.vars for local development, and should' +
-				' be uploaded to Cloudflare secrets through NPX if you are testing your worker. See README for more details')
-		}
-
-		if (env.MOMENTO_CACHE_NAME === undefined) {
-			throw new Error('MOMENTO_CACHE_NAME must be set in wrangler.toml file. See README for more details')
-		}
-
-		if (env.MOMENTO_HTTP_ENDPOINT === undefined) {
-			throw new Error('MOMENTO_HTTP_ENDPOINT must be set in wrangler.toml file. See README for more details')
-		}
-
-		const client = new MomentoFetcher(env.MOMENTO_API_KEY, env.MOMENTO_HTTP_ENDPOINT);
+		const client = new MomentoFetcher(env.MOMENTO_AUTH_TOKEN, env.MOMENTO_REST_ENDPOINT);
 		const cache = env.MOMENTO_CACHE_NAME;
-		const key = "key";
-		const value = "value";
+
+		const url = new URL(request.url);
+		const params = new URLSearchParams(url.search);
+
+		const makeRandomString = (length: number) => Math.random().toString(36).substring(2, length + 2);
+		const key = makeRandomString(10);
+		console.log(key);
+		const value = params.get('value');
+		console.log(value);
 
 		// setting a value into cache
 		const setResp = await client.set(cache, key, value);
@@ -95,10 +84,10 @@ export default {
 		const getResp = await client.get(cache, key)
 		console.log("getResp", getResp);
 
-		const deleteResp = await client.delete(cache, key);
-		console.log("deleteResp", deleteResp);
+		//const deleteResp = await client.delete(cache, key);
+		//console.log("deleteResp", deleteResp);
 
 		// deleting a value from cache
-		return new Response(JSON.stringify({ response: getResp }));
+		return new Response(key);
 	},
 };
